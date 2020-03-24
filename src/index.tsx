8b4fcc3668219e0fa99f4a4d8fe244774ce2b533
DaniAkash <s.daniakash@gmail.com>
2020-03-24 23:51:52 +0530

    removed container style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ViewProps, StyleSheet } from "react-native";
+import { View, ViewProps } from "react-native";
 
 export interface BlankSpacerProps extends ViewProps {
   height?: number;
@@ -14,16 +14,7 @@ const BlankSpacer = ({
   color: backgroundColor,
   ...otherProps
 }: BlankSpacerProps) => (
-  <View
-    style={[styles.container, { height, width, backgroundColor }, style]}
-    {...otherProps}
-  />
+  <View style={[{ height, width, backgroundColor }, style]} {...otherProps} />
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
 
 export default BlankSpacer;
